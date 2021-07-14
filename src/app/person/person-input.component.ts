3bc5836d8c233886313984b010abdcf7986c9931
Ricardo Bravo <ricardobravo@outlook.com>
2021-07-14 01:09:50 -0500

    feat: Add person input componnet css

--- src/app/person/person-input.component.ts
+++ src/app/person/person-input.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-person-input',
-  templateUrl: './person-input.component.html'
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
 })
 
 export class PersonInputComponent {}
